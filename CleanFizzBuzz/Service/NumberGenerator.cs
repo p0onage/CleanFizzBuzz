b08647bfd1ce7c1223e6d5ca40aad884de4bee78
Joshua Duxbury <work@joshduxbury.co.uk>
2024-07-03 10:38:17 +0100

    Added Number Count Test

---
+++ CleanFizzBuzz/Service/NumberGenerator.cs
@@ -0,0 +1,9 @@
+namespace CleanFizzBuzz.Service;
+
+public class NumberGenerator
+{
+    public IEnumerable<string> GetNumbers(int start, int end)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CleanFizzBuzzTests/Service/NumberGeneratorTests.cs
@@ -0,0 +1,22 @@
+using CleanFizzBuzz.Service;
+using NUnit.Framework;
+using Shouldly;
+
+namespace CleanFizzBuzzTests.Service;
+
+public class NumberGeneratorTests
+{
+ 
+    [Test]
+    public void Should_Get_Number_Count_Of_100()
+    {
+        // Arrange
+        var numberGenerator = new NumberGenerator();
+        
+        // Act
+        var numbers = numberGenerator.GetNumbers(1, 100);
+        
+        // Assert
+        numbers.Count().ShouldBe(100);
+    }
+}
