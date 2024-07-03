a3d3a5fdaf4d433a05fd3852b9da3f520832562e
Joshua Duxbury <work@joshduxbury.co.uk>
2024-07-03 11:32:00 +0100

    Printing out results to console

--- CleanFizzBuzz/Program.cs
+++ CleanFizzBuzz/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CleanFizzBuzz.Service;
+
 Console.WriteLine("Fizzbuzz in C#");
+
+var numberGenerator = new NumberGenerator();
+
+numberGenerator
+    .GetFizzBuzzNumbers(1, 100)
+    .ForEach(r => Console.WriteLine(r));
+
+Console.ReadLine();
+return 0;
--- CleanFizzBuzz/Service/NumberGenerator.cs
+++ CleanFizzBuzz/Service/NumberGenerator.cs
@@ -1,12 +1,34 @@
+using CleanFizzBuzz.NumberParsers;
+
 namespace CleanFizzBuzz.Service;
 
 public class NumberGenerator
 {
-    public IEnumerable<string> GetFizzBuzzNumbers(int start, int end)
+    public List<string> GetFizzBuzzNumbers(int start, int end)
     {
-        var inputRange = Enumerable.Range(start, end).ToList()
+        var fizzBuzzResults = new List<string>();
+        var inputRange = Enumerable.Range(start, end).ToList();
+        var numberParsers = Parsers();
             
+        inputRange.ForEach(n =>
+        {
+            var validParser = numberParsers.First(p => n % p.Divisor == 0);
+            fizzBuzzResults.Add(validParser.Parse(n));
+        });
             
-        return inputRange.Select(x => x.ToString());
+        return fizzBuzzResults;
+    }
+    
+    private List<INumberParser> Parsers()
+    {
+            var interfaceType = typeof(INumberParser);
+            
+            return AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(x => x.GetTypes())
+                    .Where(x => interfaceType.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+                    .Select(x => Activator.CreateInstance(x))
+                    .Cast<INumberParser>()
+                    .OrderByDescending(p => p.Divisor)
+                    .ToList();
     }
 }
